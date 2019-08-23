d72c57affe6351f906b5cefc9d1b6001f7f4569c
Jennie Ji <jennie.ji@hotmail.com>
2019-08-23 17:55:34 +0800

    cli-file-tree

---
+++ packages/cli-file-tree/__tests__/git-changes-affected.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const gitChangesAffected = require('..');
+
+describe('git-changes-affected', () => {
+    it('needs tests');
+});
---
+++ packages/cli-file-tree/src/printFileTree.ts
@@ -0,0 +1,26 @@
+const FILE_PREFIX = '\u2501 ';
+const LAST_PREFIX = '  \u2515';
+const HAS_NEXT_PREFIX = '  \u2506';
+const MID_PREFIX = '  \u251D'
+
+type TreeNode = Map<string, any> | void;
+type Tree = Map<string, TreeNode>;
+
+function makePrefix(depth: string) {
+  return depth.replace(/1/g, HAS_NEXT_PREFIX).replace(/0/g, '   ');
+}
+
+export default function printFileTree(tree: Tree, depth: string = '') {
+  const prefix = makePrefix(depth);
+
+  let index = -1;
+  tree.forEach((children: TreeNode, key: string) => {
+    const isLast = ++index === tree.size - 1;
+    console.log(`${prefix}${isLast ? LAST_PREFIX : MID_PREFIX}${FILE_PREFIX}${key}`);
+    if (children instanceof Map) {
+      const nextDepth =
+        depth + (isLast ? '0' : '1');
+      printFileTree(children, nextDepth);
+    }
+  });
+}
