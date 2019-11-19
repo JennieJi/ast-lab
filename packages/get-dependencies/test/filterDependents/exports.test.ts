7eb055af397fcd064fe98964ccd3eb933a4b8561
Jennie Ji <jennie.ji@shopee.com>
2019-11-19 22:13:31 +0800

    Fix path in exports test

--- packages/get-dependencies/test/filterDependents/exports.test.ts
+++ packages/get-dependencies/test/filterDependents/exports.test.ts
@@ -6,11 +6,11 @@ enumerateArgsTestFunction(
   filterDependents.bind(null, 'exports'),
   configArgs()
   .arg('sources', [
-    ['./exportNamed.js', './exportDefault.js', 'exportAllFrom.js'],
-    ['./exportNamed.js', './exportDefault.js',],
-    ['./exportNamed.js', './exportDefault.js', 'exportDefaultAliasFrom.js'],
-    ['./exportNamed.js', './exportDefault.js', 'exportNamedFrom.js'],
-    ['./exportNamed.js', './exportDefault.js', 'noExports.js']
+    ['exportNamed.js', 'exportDefault.js', 'exportAllFrom.js'],
+    ['exportNamed.js', 'exportDefault.js',],
+    ['exportNamed.js', 'exportDefault.js', 'exportDefaultAliasFrom.js'],
+    ['exportNamed.js', 'exportDefault.js', 'exportNamedFrom.js'],
+    ['exportNamed.js', 'exportDefault.js', 'noExports.js']
   ])
   .arg('targets', [
     { './exportNamed': ['a'] },
