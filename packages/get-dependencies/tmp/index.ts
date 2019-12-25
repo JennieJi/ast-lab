971bd2c831e6bf74e0751f47630a2d489e080dab
Jennie Ji <jennie.ji@hotmail.com>
2019-12-25 08:57:04 +0800

    remove tmp files

--- packages/get-dependencies/tmp/index.ts
+++
@@ -1,9 +0,0 @@
-import { default as filterDependents } from './filterDependents';
-
-export { default as getExports } from './es6Detect/getExports';
-export { default as hasExt } from './hasExt';
-export { default as createResolver } from './createResolver';
-
-export * from './types';
-export { filterDependents };
-export default filterDependents;
