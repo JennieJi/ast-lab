d38de0f56907182eb49826ab2f4844cd93e02ab6
Jennie Ji <jennie.ji@shopee.com>
2019-12-25 08:57:04 +0800

    remove tmp files

--- packages/get-dependencies/tmp/constants.ts
+++
@@ -1,4 +0,0 @@
-export const ALL_MODULES = '*';
-export const DEFAULT = 'default';
-export const ALL_EXPORTS: true = true;
-export const DEFAULT_EXTENSIONS = ['.js', '.ts', '.jsx', '.tsx'];
--- packages/get-dependencies/tmp/hasExt.ts
+++
@@ -1,5 +0,0 @@
-function hasExt(file: string, extensions?: string[]) {
-  const reg = extensions ? new RegExp(`(${extensions.join('|').replace(/\./g, '\\.')})$`, 'i') : /\.\w+/;
-  return reg.test(file);
-}
-export default hasExt;
