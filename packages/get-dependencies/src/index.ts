1990966c1a19836da79d5cee03a5df7cc6cb1ba8
Jennie Ji <jennie.ji@hotmail.com>
2019-12-28 09:40:44 +0000

    get-dependencies: add exports

---
+++ packages/get-dependencies/src/index.ts
@@ -0,0 +1,6 @@
+import getDependencies from './getDependencies';
+
+export { default as fileDepMap } from './fileDepMap';
+export { default as mergeDepMap } from './mergeDepMap';
+export { default as visitDepMap } from './visitDepMap';
+export default getDependencies;
