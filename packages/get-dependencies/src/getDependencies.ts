2db5407c4dd0164c81cda0209ef66d97677de0c8
Jennie Ji <jennie.ji@hotmail.com>
2019-12-28 09:40:44 +0000

    get-dependencies: add exports

---
+++ packages/get-dependencies/src/getDependencies.ts
@@ -0,0 +1,8 @@
+import { Options, Entry } from 'ast-lab-types';
+import mergeDepMap from './mergeDepMap';
+import visitDependencyMap from './visitDepMap';
+
+export default async function getDependencies(sources: string[], entries: Entry[], opts: Options = {}) {
+  const depMap = await mergeDepMap(sources, opts);
+  return visitDependencyMap(depMap, entries);
+}
