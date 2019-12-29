a9cf115583c4a46ec2621720d90696719b28769a
Jennie Ji <jennie.ji@hotmail.com>
2019-12-29 09:40:04 +0000

    es-stats: add loc info

--- packages/es-stats/src/index.ts
+++ packages/es-stats/src/index.ts
@@ -1,1 +1,5 @@
+export { default as createExportVisitors } from './visitors/exports';
+export { default as createImportVisitors } from './visitors/imports';
+export { default as createRootRelationVisitors } from './visitors/rootRelation';
+export { default as  mergeVisitors } from './mergeVisitors';
 export { default } from './getStats';
