ddeab2c6086626b180bf162193242b5051db5ad6
Jennie Ji <jennie.ji@shopee.com>
2020-01-01 14:35:06 +0800

    hunt-affected: export result type

--- packages/hunt-affected/src/index.ts
+++ packages/hunt-affected/src/index.ts
@@ -2,5 +2,5 @@ import huntAffected from './huntAffected';
 
 export { default as fileDepMap } from './fileDepMap';
 export { default as mergeDepMap } from './mergeDepMap';
-export { default as visitDepMap } from './visitDepMap';
+export { default as visitDepMap, Visited as Affected } from './visitDepMap';
 export default huntAffected;
