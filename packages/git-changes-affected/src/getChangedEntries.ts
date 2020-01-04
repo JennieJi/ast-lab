d3d01236ceb7efefbf195dd4b169d7d8253f86c5
Jennie Ji <jennie.ji@shopee.com>
2020-01-05 01:17:08 +0800

    git-changes-affected: fix getChangedEntries to find affected exports from privates

--- packages/git-changes-affected/src/getChangedEntries.ts
+++ packages/git-changes-affected/src/getChangedEntries.ts
@@ -1,13 +1,29 @@
 import { parse, ParserOptions } from '@babel/parser';
-import traverse from '@babel/traverse';
-import { createExportVisitors } from 'es-stats';
+import { File, SourceLocation } from '@babel/types';
+import { extractStats, getPrivateMemberNames } from 'es-stats';
 import _debug from 'debug';
 import getAbsolutePath from './getAbsolutePath';
-import { Exports, Entry } from 'ast-lab-types';
+import { Entry, MemberRef, MemberRelation } from 'ast-lab-types';
 import { Change } from './types';
 
 
 const debug = _debug('git-changes-affected:entries');
+
+type Declare = MemberRef & { loc: SourceLocation };
+/** @todo improve precision of private declaration location */
+function locatePrivateDeclares(ast: File): Declare[] {
+  return ast.program.body.reduce((locs, d) => {
+    const refs = getPrivateMemberNames(d);
+    const { loc } = d;
+    if (!refs || !refs.length || !loc) { return locs; }
+    return locs.concat(
+      refs.map(ref => ({
+        ...ref,
+        loc
+      }))
+    );
+  }, [] as Declare[]);
+}
 
 export default function getChangedEntries(changes: Change[], parserOptions?: ParserOptions | null) {
   return changes.reduce((res, { file, content, changed }) => {
@@ -15,44 +31,66 @@ export default function getChangedEntries(changes: Change[], parserOptions?: Par
       return res;
     }
     const filePath = getAbsolutePath(file);
-    const exported = { members: [] } as Exports;
+    let ast;
     try { 
-      const ast = parse(content, { 
+      ast = (parse(content, { 
         ...(parserOptions || {}),
         sourceType: 'module'
-      });
-      // @ts-ignore
-      traverse(ast, createExportVisitors(exported));
-      debug('file:', file);
-      debug('exported:', exported);
+      }) as File);
     } catch(e) {
-      console.warn(`@bable/parser parsing ${filePath} failed!`);
-      console.trace();
-      console.warn(e);
-      console.warn('Parser options:', parserOptions);
-      console.warn('File content:');
-      console.warn(content);
+      console.warn(`@bable/parser parsing ${filePath} failed! (${e.message}) Parser options: ${JSON.stringify(parserOptions)}`);
+      console.warn('File content >>> \n', content);
       return res;
     }
-    let iExported = 0;
+    const stats = extractStats(ast);
+    debug(`${file} stats: ${JSON.stringify(stats, null, 2)}`);
+    const declareLoc = [
+      ...locatePrivateDeclares(ast),
+      ...(stats.exports.members as Declare[]),
+      ...(stats.imports as Declare[])
+    ];
+    debug(`${file} declareLoc: ${JSON.stringify(declareLoc, null, 2)}`);
+    const exported = new Set(stats.exports.members.map(({ alias }) => alias));
+    const affectExports = {} as MemberRelation;
+    exported.forEach(name => {
+      const expanded = new Set(stats.relations[name] || []);
+      expanded.forEach(m => {
+        if (m === name) { return; }
+        affectExports[m] = [
+          ...(affectExports[m] || []),
+          name
+        ];
+        if (stats.relations[m]) {
+          stats.relations[m].forEach(r => expanded.add(r));
+        }
+      });
+    });
+
+    let iDeclare = 0;
     const changedExports = changed.reduce(
-      (res, { start: startLine, end: endLine }) => {
-        while (iExported < exported.members.length) {
-          const ex = exported.members[iExported];
-          iExported++;
-          if (!ex.loc) { continue; }
-          const { start, end } = ex.loc;
+      (ex, { start: startLine, end: endLine }) => {
+        while (iDeclare < declareLoc.length) {
+          const { loc, alias } = declareLoc[iDeclare];
+          iDeclare++;
+          if (!loc) { continue; }
+          const { start, end } = loc;
           if (endLine < start.line) { 
-            return res;
-          } else if (startLine <= end.line) {
-            res.push(ex.alias);
+            return ex;
+          }
+          if (startLine <= end.line) {
+            if (exported.has(alias)) {
+              ex.push(alias);
+            } else {
+              ex = ex.concat(affectExports[alias]);
+            }
           }
         }
-        return res;
+        return ex;
       }, 
       [] as string[]
     );
-    return res.concat(changedExports.map(name => ({
+    return res.concat(
+      Array.from(new Set(changedExports)).map(name => ({
         source: filePath,
         name
       }))
